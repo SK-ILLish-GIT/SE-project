54a3971658c5bbef9e6f9b21a58c908701bb28ef
SK-ILLish-GIT <sksahilparvez2000@gmail.com>
2023-03-22 20:40:25 +0530

    homepage restructureed

--- app.js
+++ app.js
@@ -1,72 +1,71 @@
-const express=require("express");
-const bodyParser=require("body-parser");
-const app=express();
-const mongoose=require("mongoose");
-
-app.use(bodyParser.urlencoded({extended:true}));
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public'));
-
+app.set('view engine', 'ejs');
 
 //////////////////////////////Mongoose connection and Schema////////////////////////////////////////////////////////
-const connect= async ()=> {
-    try{
+const connect = async () => {
+    try {
         await mongoose.connect('mongodb://127.0.0.1:27017/PortfolioDB');
         console.log("Server Connected");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 }
 connect();
-const userSchema=mongoose.Schema({
-    email:{type:String,required:true},
-    password:{type:String,required:true}
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true }
 });
-const Users=mongoose.model("user",userSchema);
+const Users = mongoose.model("user", userSchema);
 
-// const Zia=new User({
-//     email:"IIB2021027@iiita.ac.in",
-//     password:"027"
+// const Zia = new Users({
+//     email: "IIB2021027@iiita.ac.in",
+//     password: "027"
 // });
-// const Sahil=new User({
-//     email:"IIB2021038@iiita.ac.in",
-//     password:"038"
+// const Sahil = new Users({
+//     email: "IIB2021038@iiita.ac.in",
+//     password: "038"
 // });
-// User.insertMany([Zia,Sahil]).catch((error)=>{
-//     console.log("Error in adding admins - "+error);
+// Users.insertMany([Zia, Sahil]).catch((error) => {
+//     console.log("Error in adding admins - " + error);
 // });
 
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-app.get("/",(req,res)=>{
-    res.sendFile(__dirname+"/LogIn/login.html");
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/LogIn/login.html");
 });
-app.get("/sign-in",(req,res)=>{
-    
-    res.sendFile(__dirname+"/LogIn/sign_in.html");
+app.get("/sign-in", (req, res) => {
+
+    res.sendFile(__dirname + "/LogIn/sign_in.html");
 });
 
 
-app.post("/sign-in",(req,res)=>{
+app.post("/sign-in", (req, res) => {
     // console.log(req.body);
-    const userEmail=req.body.email,userPassword=req.body.password;
-    const findUser=async ()=>{
-        try{
-            const user=await Users.findOne({email:userEmail});
-            if(user===null)
-            res.send("<h1>NO such user</h1>");
-            else
-            {
-                if(user.password===userPassword)
-                res.send("<h1>Singed in sucessfully</h1>");
+    const userEmail = req.body.email, userPassword = req.body.password;
+    const findUser = async () => {
+        try {
+            const user = await Users.findOne({ email: userEmail });
+            if (user === null)
+                res.send("<h1>NO such user</h1>");
+            else {
+                if (user.password === userPassword)
+                    res.send("<h1>Singed in sucessfully</h1>");
                 else
-                res.send("<h1>Wrong password</h1>");
+                    res.send("<h1>Wrong password</h1>");
             }
         }
-        catch(error){
-            console.log("Error in finding user - "+error);
+        catch (error) {
+            console.log("Error in finding user - " + error);
         }
 
     };
@@ -78,6 +77,6 @@ app.post("/sign-in",(req,res)=>{
 
 
 
-app.listen(3000,(req,res)=>{
+app.listen(3000, (req, res) => {
     console.log("Server running");
 });
