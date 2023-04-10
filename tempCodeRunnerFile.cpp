37b7880a6ce0ed5d948940e949e30f6c39d444d6
Rhythm1027 <95673596+Rhythm1027@users.noreply.github.com>
2023-04-10 21:16:33 +0530

    Search buton bug removed

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,70 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
 typedef long long ll;
+bool isPerfectSquare(long double x){if (x >= 0) {long long sr = sqrt(x);return (sr * sr == x);}return false;}
 ll hcf(ll a,ll b){if(a==0){return b;}hcf(b%a,a);}
 ll lcm(ll a,ll b){return (a/hcf(a,b))*b;}
 
-set<ll> s;
-vector<ll> fibonacci;
-
-bool isFibonacci(int n)
-{
-    return s.find(n)!=s.end();
-}
-
-bool helperleft(int x,int y,int r,int c){
-    // if(x-2<0 || y-1<0 || r-x<0 || c-y<0){
-    //     return false;
-    // }
-    return isFibonacci(x-2) && isFibonacci(y-1) && isFibonacci(r-x) && isFibonacci(c-y);
-}
-bool helperright(int x,int y,int r,int c){
-    // if(x+2>r || y-1<0 || r-x<0 || c-y<0){
-    //     return false;
-    // }
-    return isFibonacci(x+2) && isFibonacci(y-1) && isFibonacci(r-x) && isFibonacci(c-y);
-}
-bool helperup(int x,int y,int r,int c){
-    // if(x-1<0 || y-2<0 || r-x<0 || c-y<0){
-    //     return false;
-    // }
-    return isFibonacci(x-1) && isFibonacci(y-2) && isFibonacci(r-x) && isFibonacci(c-y);
-}
-bool helperdown(int x,int y,int r,int c){
-    // if(x-1>r || y-1<0 || r-x<0 || c-y-1<0){
-    //     return false;
-    // }
-    return isFibonacci(x-1) && isFibonacci(y-1) && isFibonacci(r-x) && isFibonacci(c-y-1);
+bool cmp(pair<ll,ll> &a,pair<ll,ll> &b){
+    return a.first>b.first;
 }
 
 void solve(){
-    int n,x,y;
-    cin>>n>>x>>y;
-    int h = fibonacci[n];
-    int w = fibonacci[n+1];
-    bool ans = helperleft(x,y,w,h) || helperright(x,y,w,h) || helperup(x,y,w,h) || helperdown(x,y,w,h);
-    if(ans){
-        cout<<"YES"<<endl;
+    ll n,k;
+    cin>>n>>k;
+    multiset<ll> s;
+    vector<pair<ll,ll>> arr;
+    for(ll i=0;i<n;i++){
+        ll m,v;
+        cin>>m>>v;
+        arr.push_back({v,m});
     }
-    else{
-        cout<<"NO"<<endl;
+    for(int i=0;i<k;i++){
+        int m;
+        cin>>m;
+        s.insert(m);
     }
+    sort(arr.begin(),arr.end(),cmp);
+    ll ans=0;
+    ll num=0;
+    for(ll i=0;num<=k && i<n;i++){
+        auto it = lower_bound(s.begin(),s.end(),arr[i].second);
+        if(it!=s.end()){
+            ans+=arr[i].first;
+            num+=1;
+            s.erase(it);
+        }
+    }
+    cout<<ans<<endl;
 }
+
 
 int main(){
 ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-ll a=1,b=1;
-for(int i=0;i<44;i++){
-    s.insert(a+b);
-    fibonacci[i]=a+b;
-    int temp=a+b;
-    a=b;
-    b=temp;
-}
 ll t=1;
-cin>>t;
+// cin>>t;
 while(t--){
-    solve();
+solve();
 }
 return 0;
 }
